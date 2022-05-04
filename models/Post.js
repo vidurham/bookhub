3d927b2a1bd817198b8c856a8ba7125e3dddb421
Vinny D <vdurham@prideseals.com>
2022-05-04 19:12:00 -0400

    connection changed to config

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../config/config');
 // create our Post model
 class Post extends Model {
   static upvote(body, models) {
