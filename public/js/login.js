38862e78af32dc27b7ac198ffec6c7022dcf6c9d
Vinny D <vdurham@prideseals.com>
2022-05-04 19:47:48 -0400

    server now shows login page

--- public/js/login.js
+++ public/js/login.js
@@ -2,4 +2,28 @@
 const loginFormHandler = async function (event) {
   event.preventDefault();
 
-};
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+  
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
+  } 
+}
+  
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+  
+
