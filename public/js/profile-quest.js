b55c44ba73ef8a6519e0e5a0bc0885b476eba64c
Vinny D <vdurham@prideseals.com>
2022-05-15 17:14:15 -0400

    search book fix

--- public/js/profile-quest.js
+++ public/js/profile-quest.js
@@ -5,6 +5,7 @@ function collectChecks(event){
   const genreArr = document.querySelectorAll('.form-check-input:checked')
   const checkedArr = Array.from(genreArr).map(element => element.name);
   console.log(checkedArr);
+  document.location.replace("/user-feed")
 }
 
 finishSetUpBtn.addEventListener("click", collectChecks);
--- public/js/search-book.js
+++ public/js/search-book.js
@@ -5,12 +5,11 @@ const url = "http://openlibrary.org/search.json?title=the+lord+of+the+rings"
 var searchByTitle = function() {
     fetch(url)
         .then(response => response.json().then(data => {
-            document.location.replace('/search-page')
             console.log(data.docs)
             for (var i = 0; i < 20; i++) {
                 const bookCover = data.docs[i].cover_i
                 const bookTitle = data.docs[i].title
-                const bookAuthor = data.docs[i].author
+                const bookAuthor = data.docs[i].author_name[0]
                 const book = document.createElement("img");
                 book.src = "https://covers.openlibrary.org/b/id/240727-M.jpg"
                 books.append(book)
