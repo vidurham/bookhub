ab54b38f59cdd6df3cc1db8d4bfa71a230b14d2e
Vinny D <vdurham@prideseals.com>
2022-05-16 00:03:08 -0400

    search in place

--- public/js/search-book.js
+++ public/js/search-book.js
@@ -1,20 +1,10 @@
-var searchResultsEl = document.querySelector("#search-result")
-
-const url = "http://openlibrary.org/search.json?title=the+lord+of+the+rings"
+const res = require("express/lib/response");
 
 var searchByTitle = function() {
-    fetch(url)
-        .then(response => response.json().then(data => {
-            console.log(data.docs)
-            for (var i = 0; i < 20; i++) {
-                const bookCover = data.docs[i].cover_i
-                const bookTitle = data.docs[i].title
-                const bookAuthor = data.docs[i].author_name[0]
-                const book = document.createElement("img");
-                book.src = "https://covers.openlibrary.org/b/id/240727-M.jpg"
-                books.append(book)
-            }
-        }))
+    const search = document.querySelector('input[id="searchBar"]').value;
+    const query = search.split(" ").join("+");
+    console.log(query)
+    module.exports = query
 }
 
 document.querySelector('#search-btn').addEventListener('click', searchByTitle);
